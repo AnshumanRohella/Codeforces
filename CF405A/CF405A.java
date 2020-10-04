8e7d8bdde8fbbf4fdb1517cd64ba72fa9bcce7e8
AnshumanRohella <rohella.anshuman@gmail.com>
2020-10-04 22:29:39 +0900

    New probs

--- CF405A/CF405A.java
+++ CF405A/CF405A.java
@@ -1,9 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class FastScanner {
+
+public class CF405A {
+    public static void main(String... args){
+        FastScanner sc = new FastScanner();
+        int size = sc.nextInt();
+        int[]  arr= new int[size];
+        int max =0;
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+            max = Integer.max(arr[i],max);
+        }
+
+        int[] buffer = new int[max+1];
+
+        for(Integer val: arr){
+            buffer[val]++;
+        }
+        for(int i=1;i<buffer.length;i++){
+            buffer[i]=buffer[i]+buffer[i-1];
+        }
+        for(int i=buffer.length-1;i>0;i--){
+            buffer[i]=buffer[i-1];
+        }
+        buffer[0]=0;
+
+        int[] res = new int[size];
+        // System.out.println(Arrays.toString(res));
+        for(int i=0;i<size;i++){
+            res[buffer[arr[i]]] = arr[i];
+            buffer[arr[i]]++;
+        }
+        // System.out.println(Arrays.toString(res));
+
+        for(Integer val: res){
+            System.out.printf("%d ",val);
+        }
+    }
+}
+
+class FastScanner {
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st=new StringTokenizer("");
     String next() {
@@ -22,9 +62,4 @@ public class FastScanner {
     }
 }
 
-public class CF405A {
-    public static void main(String... args){
-        
-    }
-}
 
